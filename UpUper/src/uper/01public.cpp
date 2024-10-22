4b7d90633385d84018c6359d026cb1ecdcdeb83f
Fangled-xiang <3102467705@qq.com>
2024-10-22 17:26:59 +0800

    Add files via upload

---
+++ UpUper/src/uper/01public.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+class student
+{
+public:
+    string id;
+    string name;
+
+public:
+    void show()
+    {
+        cout<< id <<"-"<<name<<endl;
+    }
+};
+
+
+/*
+int main()
+{
+    student s;
+    s.id = "012002108";
+    s.name = "·½Ïò";
+
+    s.show();
+}
+*/
